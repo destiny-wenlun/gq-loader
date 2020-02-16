3a8f88afb8ded9262ce1ccad42526ac0c51ff7fb
wenlun <1070586554@qq.com>
2020-02-16 23:56:02 +0800

    处理fragment，将query或mutation中使用到的片段名替换成片段主体

--- demo/index.js
+++ demo/index.js
@@ -1,15 +1,20 @@
 import userGql from './test1.gql';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+window.gajax = function (data, options) {
+  console.log(data, options);
+};
+
 console.log(userGql);
 async function query() {
-  const user = await userGql({ name: 'bob' });
-  console.log('user', user);
+  const user = await userGql.get({ name: 'bob' });
+  // console.log('user', user);
 }
 
 async function update() {
   const user = await userGql.update({ form: { age: 25 } });
-  console.log('after update user:', user);
+  // console.log('after update user:', user);
 }
 
 function App() {
